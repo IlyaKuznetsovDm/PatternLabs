278e041a708368a32e17cc4bd6bdb0f5c22b8ec6
User <User@DESKTOP-K0EQ5UJ>
2023-12-05 09:27:35 +0200

    Добавьте файлы проекта.

---
+++ Prac1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Character
+{
+    protected char symbol;
+    protected string font;
+    protected int size;
+    protected string color;
+
+    public abstract void Display(int row, int column);
+}
+
+class ConcreteCharacter : Character
+{
+    public ConcreteCharacter(char symbol, string font, int size, string color)
+    {
+        this.symbol = symbol;
+        this.font = font;
+        this.size = size;
+        this.color = color;
+    }
+
+    public override void Display(int row, int column)
+    {
+        Console.WriteLine("Symbol: " + symbol + ", Font: " + font + ", Size: " + size + ", Color: " + color + ", Row: " + row + ", Column: " + column);
+    }
+}
+
+class CharacterFactory
+{
+    private Dictionary<char, Character> characters = new Dictionary<char, Character>();
+
+    public Character GetCharacter(char key, string font, int size, string color)
+    {
+        if (characters.ContainsKey(key))
+        {
+            return characters[key];
+        }
+        else
+        {
+            Character character = new ConcreteCharacter(key, font, size, color);
+            characters.Add(key, character);
+            return character;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CharacterFactory factory = new CharacterFactory();
+        Character char1 = factory.GetCharacter('a', "Arial", 12, "Black");
+        Character char2 = factory.GetCharacter('b', "Times New Roman", 14, "Red");
+        Character char3 = factory.GetCharacter('a', "Arial", 12, "Black"); 
+        char1.Display(1, 1);
+        char2.Display(2, 3);
+        char3.Display(3, 5);
+    }
+}
---
+++ Prac2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class OpticalSystem
+{
+    private OpticalSystemState state;
+
+    public OpticalSystem()
+    {
+        state = new InitialState(); 
+    }
+
+    public void SetState(OpticalSystemState state)
+    {
+        this.state = state;
+    }
+
+    public void PerformOperation()
+    {
+        state.PerformOperation(this);
+    }
+}
+
+interface OpticalSystemState
+{
+    void PerformOperation(OpticalSystem system);
+}
+
+class InitialState : OpticalSystemState
+{
+    public void PerformOperation(OpticalSystem system)
+    {
+        Console.WriteLine("Расчет лучей...");
+        system.SetState(new VisualizationState()); 
+    }
+}
+
+class VisualizationState : OpticalSystemState
+{
+    public void PerformOperation(OpticalSystem system)
+    {
+        Console.WriteLine("Визуализация системы....");
+        system.SetState(new AnotherState());
+    }
+}
+
+class AnotherState : OpticalSystemState
+{
+    public void PerformOperation(OpticalSystem system)
+    {
+        Console.WriteLine("Выполнение другой операции.....");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        OpticalSystem opticalSystem = new OpticalSystem();
+
+        opticalSystem.PerformOperation(); 
+        opticalSystem.PerformOperation(); 
+        opticalSystem.PerformOperation(); 
+    }
+}
